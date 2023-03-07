1ff35ff99f41a1506d5527eafe8a4b2c7f36bc9d
Lakshya <Lakshya.Agarwal@lntindotech.com>
2023-03-07 17:23:07 +0530

    initial commit

---
+++ src/main/java/com/example/youtubeclone/controller/VideoController.java
@@ -0,0 +1,44 @@
+package com.example.youtubeclone.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.youtubeclone.Utils.CommonConstants;
+import com.example.youtubeclone.service.VideoService;
+
+@RestController
+@RequestMapping("videos")
+public class VideoController {
+	
+	@Autowired
+	private VideoService videoService;
+
+	@PostMapping(path = "/upload")
+	public ResponseEntity uploadVideo( @RequestParam("file") MultipartFile file) {
+		Map<String, Object> result = new HashMap<>();
+		Boolean status = videoService.uploadFile(file);
+		if (Boolean.TRUE.equals(status)) {
+			result.put(CommonConstants.STATUS, CommonConstants.SUCCESS);
+		} else
+			result.put(CommonConstants.STATUS, CommonConstants.NO_DATA_FOUND);
+		return ResponseEntity.status(HttpStatus.SC_OK).body(result);
+		
+	}
+	
+	 @PostMapping("/jira-webhook/{pcmId}")
+	    public void handleWebhook(@RequestBody String payload,@PathVariable("pcmId") Integer pcmId) {
+		 	System.out.println("Received for pcm " + pcmId);
+	        System.out.println("Received Jira webhook: " + payload);
+	    }
+}
---
+++ src/main/java/com/example/youtubeclone/service/VideoService.java
@@ -0,0 +1,9 @@
+package com.example.youtubeclone.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface VideoService {
+
+	 Boolean uploadFile(MultipartFile File) ;
+	
+}
