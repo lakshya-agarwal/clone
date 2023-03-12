f3b1d9ad19643184870aeb7e761c42e8112843bf
Lakshya <Lakshya.Agarwal@lntindotech.com>
2023-03-12 11:21:54 +0530

    Api Gateway and Eureka Added

--- kafka-producer/src/main/java/org/kafka/producer/Controller/DataProducerController.java
+++ kafka-producer/src/main/java/org/kafka/producer/Controller/DataProducerController.java
@@ -2,8 +2,6 @@ package org.kafka.producer.Controller;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import org.common.utilities.constants.CommonConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -13,9 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.common.utilities.constants.CommonConstants;
 
 @RestController
-@RequestMapping("webhooks")
+@RequestMapping("/webhooks")
 public class DataProducerController {
 
 	private static final Logger logger = LoggerFactory.getLogger(DataProducerController.class);
